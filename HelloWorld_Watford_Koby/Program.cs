d53d761a0a3917b0e76f0e8fe6c04366ca982eb4
Koby Watford <kobywatford8@icloud.com>
2025-02-11 14:29:18 -0600

    Initial commit

---
+++ HelloWorld_Watford_Koby/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Display a welcome message
+        Console.WriteLine("Welcome to the program!");
+
+        // Ask the user for their first name
+        Console.Write("Please enter your first name: ");
+        string firstName = Console.ReadLine(); // Store the input in firstName variable
+
+        // Ask the user for their last name
+        Console.Write("Please enter your last name: ");
+        string lastName = Console.ReadLine(); // Store the input in lastName variable
+
+        // Print a personalized greeting
+        Console.WriteLine($"Hello, {firstName} {lastName}!");
+    }
+}
